ecbf2e7effb68172f60702e6d809f9a41e3d1b60
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-08-09 22:18:53 -0500

    Add webp, animated gif support (#2)

--- ios/Plugin/StashMedia.swift
+++ ios/Plugin/StashMedia.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
 import Photos
+import SDWebImage
 
 class StashMedia {
     func copyPhotoToClipboard(from imageURLString: String, completion: @escaping (Bool, String) -> Void) {
@@ -22,9 +23,26 @@ class StashMedia {
     }
 
     func saveImageToPhotoLibrary(from imageURL: URL, completion: @escaping (Bool, String) -> Void) {
-        DispatchQueue.global(qos: .background).async {
-            if let imageData = try? Data(contentsOf: imageURL),
-               let image = UIImage(data: imageData) {
+        let options: SDWebImageDownloaderOptions = [.preloadAllFrames]
+
+        SDWebImageDownloader.shared.downloadImage(with: imageURL, options: options, progress: nil) { (image, data, error, _) in
+            if let error = error {
+                completion(false, "Failed to download image: \(error.localizedDescription)")
+                return
+            }
+
+            guard let image = image else {
+                completion(false, "Failed to download image")
+                return
+            }
+
+            guard let imageData = data else {
+                completion(false, "Failed to download image data from the URL")
+                return
+            }
+
+            // Non-anmated image
+            if image.sd_isAnimated != true {
                 PHPhotoLibrary.shared().performChanges {
                     PHAssetChangeRequest.creationRequestForAsset(from: image)
                 } completionHandler: { success, error in
@@ -36,8 +54,35 @@ class StashMedia {
                         completion(false, "Failed to save image")
                     }
                 }
-            } else {
-                completion(false, "Failed to fetch image data")
+
+                return
+            }
+
+            let originalFileName = imageURL.lastPathComponent
+            var fileURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(originalFileName)
+
+            // Append .gif to the end of the file name
+            if !originalFileName.lowercased().hasSuffix(".gif") {
+                fileURL.deletePathExtension()
+                fileURL.appendPathExtension("gif")
+            }
+
+            do {
+                try imageData.write(to: fileURL)
+                PHPhotoLibrary.shared().performChanges {
+                    PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: fileURL)
+                } completionHandler: { success, error in
+                    if success {
+                        completion(true, "Image saved to photo library")
+                        try? FileManager.default.removeItem(at: fileURL)
+                    } else if let error = error {
+                        completion(false, "Failed to save image: \(error.localizedDescription)")
+                    } else {
+                        completion(false, "Failed to save image")
+                    }
+                }
+            } catch {
+                completion(false, "Failed to write image data to file: \(error.localizedDescription)")
             }
         }
     }
